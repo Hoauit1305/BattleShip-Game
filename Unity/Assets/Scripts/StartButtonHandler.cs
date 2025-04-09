309cc2fc442f9370fa2ab35a153b19f14f6cbac5
dhh <hoaemail1305@gmail.com>
2025-04-09 21:35:34 +0700

    up thu muc unity

---
+++ Unity/Assets/Scripts/StartButtonHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonHandler : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene("AuthScene");
+    }
+}
