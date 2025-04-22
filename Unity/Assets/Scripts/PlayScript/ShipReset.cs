5b8239e1c05d5831116140f61c31a0322d0ea380
dhh <hoaemail1305@gmail.com>
2025-04-22 16:46:26 +0700

    them chuc nang reset vi tri tau cho playScene

---
+++ Unity/Assets/Scripts/PlayScript/ShipReset.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ShipReset : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+    void Start()
+    {
+        // Lưu lại vị trí và hướng ban đầu khi game bắt đầu
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+    }
+
+    public void ResetShip()
+    {
+        transform.position = initialPosition;
+        transform.rotation = initialRotation;
+    }
+}
